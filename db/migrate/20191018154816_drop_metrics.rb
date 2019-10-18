8e06686b86ee0caeded44037cd7a1603da28933a
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-10-18 17:49:32 +0200

    clear tables

---
+++ db/migrate/20191018154816_drop_metrics.rb
@@ -0,0 +1,4 @@
+class DropMetrics < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_203351) do
+ActiveRecord::Schema.define(version: 2019_10_18_154816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
