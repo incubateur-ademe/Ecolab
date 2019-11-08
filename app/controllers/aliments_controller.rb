3162c499f7fc5f56f856b42fff0bf75e556f6626
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-11-08 16:21:12 +0100

    ok for tri

--- app/controllers/aliments_controller.rb
+++ app/controllers/aliments_controller.rb
@@ -1,8 +1,12 @@
 class AlimentsController < ApplicationController
 
   def index
-    aliments = Aliment.order(created_at: :desc)
-    if params[:query].present?
+    if params[:sort].present? && params[:query].present?
+    aliments = Aliment.order(params[:sort] => :asc)
+      sql_query = "aliments.alim_nom_fr ILIKE :query OR aliments.alim_grp_nom_fr ILIKE :query OR aliments.alim_ssgrp_nom_fr ILIKE :query OR aliments.alim_ssssgrp_nom_fr ILIKE :query"
+      @aliments = aliments.where(sql_query, query: "%#{params[:query]}%")
+    elsif params[:query].present?
+      aliments = Aliment.order(created_at: :asc)
       sql_query = "aliments.alim_nom_fr ILIKE :query OR aliments.alim_grp_nom_fr ILIKE :query OR aliments.alim_ssgrp_nom_fr ILIKE :query OR aliments.alim_ssssgrp_nom_fr ILIKE :query"
       @aliments = aliments.where(sql_query, query: "%#{params[:query]}%")
     else
@@ -10,4 +14,27 @@ class AlimentsController < ApplicationController
     end
   end
 
+  # def order_by_name
+  #   aliments = Aliment.order(alim_nom_fr: :asc)
+  #   if params[:query].present?
+  #     sql_query = "aliments.alim_nom_fr ILIKE :query OR aliments.alim_grp_nom_fr ILIKE :query OR aliments.alim_ssgrp_nom_fr ILIKE :query OR aliments.alim_ssssgrp_nom_fr ILIKE :query"
+  #     @aliments = aliments.where(sql_query, query: "%#{params[:query]}%")
+  #   else
+  #     @aliments = aliments.all
+  #   end
+  #   redirect_to aliments_path
+  # end
+
+  # def order_by_score
+  #   aliments = Aliment.order(score: :desc)
+  #   if params[:query].present?
+  #     sql_query = "aliments.alim_nom_fr ILIKE :query OR aliments.alim_grp_nom_fr ILIKE :query OR aliments.alim_ssgrp_nom_fr ILIKE :query OR aliments.alim_ssssgrp_nom_fr ILIKE :query"
+  #     @aliments = aliments.where(sql_query, query: "%#{params[:query]}%")
+  #   else
+  #     @aliments = aliments.all
+  #   end
+  #   render :index
+  # end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/courses", to: "pages#courses"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :aliments, only: [:index]
+  resources :aliments, only: [:index, :order_by_name]
+  get "/order_by_name", to: "aliments#order_by_name"
   resources :fruitlegs, only: [:new, :create, :index, :update]
   resources :simulations, only: [:new, :create, :index, :update]
 end
