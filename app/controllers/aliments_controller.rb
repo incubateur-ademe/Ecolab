838cebfaf1bfa4a60eaa03cc8a651147a0c3764a
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-11-13 12:39:51 +0100

    added routes and method for aliment show + view template

--- app/controllers/aliments_controller.rb
+++ app/controllers/aliments_controller.rb
@@ -14,27 +14,7 @@ class AlimentsController < ApplicationController
     end
   end
 
-  # def order_by_name
-  #   aliments = Aliment.order(alim_nom_fr: :asc)
-  #   if params[:query].present?
-  #     sql_query = "aliments.alim_nom_fr ILIKE :query OR aliments.alim_grp_nom_fr ILIKE :query OR aliments.alim_ssgrp_nom_fr ILIKE :query OR aliments.alim_ssssgrp_nom_fr ILIKE :query"
-  #     @aliments = aliments.where(sql_query, query: "%#{params[:query]}%")
-  #   else
-  #     @aliments = aliments.all
-  #   end
-  #   redirect_to aliments_path
-  # end
-
-  # def order_by_score
-  #   aliments = Aliment.order(score: :desc)
-  #   if params[:query].present?
-  #     sql_query = "aliments.alim_nom_fr ILIKE :query OR aliments.alim_grp_nom_fr ILIKE :query OR aliments.alim_ssgrp_nom_fr ILIKE :query OR aliments.alim_ssssgrp_nom_fr ILIKE :query"
-  #     @aliments = aliments.where(sql_query, query: "%#{params[:query]}%")
-  #   else
-  #     @aliments = aliments.all
-  #   end
-  #   render :index
-  # end
-
+  def show
+    @aliment = Aliment.find(params[:id])
+  end
 end
-
