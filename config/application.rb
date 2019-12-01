c4a0b00a79f27a71ee146e1a3cdd2c0a53805b82
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-12-01 11:35:21 +0100

    ok

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Ecolab
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # config.middleware.use
 config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
