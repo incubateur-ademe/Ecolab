de6b124d0115f0d1017d34c72c0ab70690f4d058
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-01-09 13:29:26 +0100

    added linear gradient + typed.js on landing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap";
+import Typed from 'typed.js';
 
 
 $(function () {
@@ -10,3 +11,10 @@ $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
 
+
+var typed6 = new Typed('.banner_title', {
+    strings: ['Les données d’impact environnemental au plus près des usages des citoyens'],
+    typeSpeed: 30,
+    backSpeed: 0,
+    loop: false
+  });
