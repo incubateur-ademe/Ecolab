fea4a838feb223941e6c7390018bd85194931a09
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-01-17 15:14:34 +0100

    removed cursor on typed.js text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,8 @@ var typed6 = new Typed('.banner_title', {
     strings: ['Les données d’impact environnemental au plus près des usages des citoyens'],
     typeSpeed: 30,
     backSpeed: 0,
-    loop: false
+    loop: false,
+    onStringTyped: function() {
+    return elem.siblings('.typed-cursor').remove();
+    }
   });
