e0ed53efd7a85fd892670a6cc07078c6af3f37e3
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-02-07 12:18:58 +0100

    nieme modif de la seed pour peupler la bdd agri avec la version abregee du csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,26 +9,27 @@ filepath    = 'db/agribalyse.csv'
 
 CSV.foreach(filepath, csv_options) do |row|
   Aliment.create(
-    alim_code: row[6],
+    alim_code: row[0],
     alim_nom_fr: row[7],
-    alim_nom_en: row[7],
-    alim_grp_nom_fr: row[3],
-    alim_ssgrp_nom_fr: row[4],
-    alim_ssssgrp_nom_fr: row[5],
-    indic_1: row[9],
-    indic_2: row[10],
-    indic_3: row[11],
-    indic_4: row[12],
-    indic_5: row[13],
-    indic_6: row[14],
-    indic_7: row[15],
-    indic_8: row[16],
-    indic_9: row[17],
-    indic_10: row[18],
-    indic_11: row[19],
-    indic_12: row[20],
-    indic_13: row[21],
-    indic_14: row[22])
+    alim_nom_en: row[8],
+    alim_grp_nom_fr: row[4],
+    alim_ssgrp_nom_fr: row[5],
+    alim_ssssgrp_nom_fr: row[6],
+    indic_1: row[14],
+    indic_2: row[15],
+    indic_3: row[16],
+    indic_4: row[17],
+    indic_5: row[18],
+    indic_6: row[19],
+    indic_7: row[20],
+    indic_8: row[21],
+    indic_9: row[22],
+    indic_10: row[23],
+    indic_11: row[24],
+    indic_12: row[25],
+    indic_13: row[26],
+    indic_14: row[27],
+    score: row[13])
 end
 
 
