a32ce1005cec0efdf16ea2483f2a73b3f1175a27
Mael <mael.thomas@data.gouv.fr>
2020-06-03 18:39:01 +0200

    :bug: jquery et bootstrap étaient chargés à la fois en JS et en HTML
    Via webpack et via un CDN. D'où le bug du bouton de menu, et une page
    plus lourde pour rien.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,25 +1,21 @@
-import "bootstrap";
-import "bootstrap/dist/js/bootstrap.bundle";
-import Typed from 'typed.js';
-
+import Typed from "typed.js";
 
 $(function () {
-  $('[data-toggle="popover"]').popover()
-})
+  $('[data-toggle="popover"]').popover();
+});
 
+$("#myModal").on("shown.bs.modal", function () {
+  $("#myInput").trigger("focus");
+});
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-
-
-var typed6 = new Typed('.banner_title', {
-    strings: ['Les données d’impact environnemental au plus près des usages des citoyens'],
-    typeSpeed: 30,
-    backSpeed: 0,
-    loop: false,
-    onStringTyped: function() {
-    return elem.siblings('.typed-cursor').remove();
-    }
-  });
-
+var typed6 = new Typed(".banner_title", {
+  strings: [
+    "Les données d’impact environnemental au plus près des usages des citoyens",
+  ],
+  typeSpeed: 30,
+  backSpeed: 0,
+  loop: false,
+  onStringTyped: function () {
+    return elem.siblings(".typed-cursor").remove();
+  },
+});
